294dede3e88dc406bb9a5f824a622007b14a6d94
AmirTsr <githubamko@gmail.com>
2026-03-03 17:57:46 +0300

    v1.0.0

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
-func getUser(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"privet": "poka",
-	})
-}
-
 func main() {
-	g := gin.Default()
-
-	g.GET("/hi", getUser)
-	g.Run(":8080")
+	
 }
