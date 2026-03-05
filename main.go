f490ed399ef6c3bc393daa5785ae312e2eabb17f
AmirTsr <githubamko@gmail.com>
2026-03-05 14:59:03 +0300

    score

--- main.go
+++ main.go
@@ -5,7 +5,21 @@ import (
 )
 
 func main() {
-	fmt.Println("I am learning Go")
-	fmt.Println("I am learning Go")
-	fmt.Println("I am learning Go")
+	
+	var score int
+	for {
+		fmt.Print("Enter your score: ")
+		fmt.Scan(&score) 
+	 
+		switch {
+		case score >= 90 && score <= 100:
+			fmt.Println("Excellent!")
+		case score >= 75 && score < 90:
+			fmt.Println("Good!")
+		case score >= 60 && score < 75:
+			fmt.Println("Pass!")
+		default:
+			fmt.Println("Fail")
+		}
+	}
 }
